82c7c0944ed719359f97762f7e3595fd6b640cc6
GMDIV <mitchelld20@yahoo.com>
2016-11-12 16:08:18 -0600

    Created data structures containing the data for all of the cards as well as an initial start for the character component and various helper functions.

---
+++ components/character-component.js
@@ -0,0 +1,45 @@
+var React = require("react");   
+
+class Character extends React.Component {
+	constructor(characterProp){
+		super(characterProp)
+
+		this.state = {
+			character: {characterProp},//contains name, italianDescription,
+			// maxHealth, englishDescription, and ability
+			handSize: null,
+			currentHealth: null, //same as hand size
+			cardCount: 4,
+			inJail: false,
+			hasDynamite: false,
+			shootingRange: 1,
+			regularRange: 1,
+			enemySightRange: 1,
+			ability: null,
+			job: null
+		}
+	}
+
+	ability(function){
+
+	}
+
+	render(){
+
+	}
+}
+
+class App extends React.Component {
+	constructor(){
+		super()
+
+		this.state = {
+
+		}
+	}
+
+	render(){
+		<Character />
+	}
+	}
+}
